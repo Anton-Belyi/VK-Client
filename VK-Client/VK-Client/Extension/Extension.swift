31463860a76f28904fdddc6043b902b592c02b03
Anton Belyi <alterego15071970@gmail.com>
2021-12-07 21:17:50 +0300

    Add shadow

---
+++ VK-Client/VK-Client/Extension/Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Extension.swift
+//  VK-Client
+//
+//  Created by Антон Белый on 07.12.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UITableViewCell {
+    func setupShadow(view: UIView, image: UIImageView) {
+        
+    }
+}
